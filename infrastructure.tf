84a49187ff2b7fd895679f5eb2f590fcf2f86307
Daniel <danielspeixoto@outlook.com>
2018-02-20 14:43:37 -0300

    terraform

---
+++ infrastructure.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
