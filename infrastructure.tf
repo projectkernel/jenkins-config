e63f4073a0634d22f0b25f5f8d6ebfb79e707c0a
Daniel <danielspeixoto@outlook.com>
2018-02-20 15:48:02 -0300

    rename

--- infrastructure.tf
+++ infrastructure.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "jenkins" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+    command = "echo ${aws_instance.jenkins.public_ip} > ip_address.txt"
   }
 }
